e7f8d3a859348c92c6ed6f20bf7bba5af02ba806
saimer-nieves <snieves@email.neit.edu>
2021-03-02 10:18:10 -0500

    updated error for navbar

--- capstone_project/frontend/outside_index.php
+++ capstone_project/frontend/outside_index.php
@@ -17,7 +17,7 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="../Backend/../Backend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="../Backend/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
   <link href="../Backend/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="../Backend/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
--- capstone_project/includes/header.php
+++ capstone_project/includes/header.php
@@ -10,23 +10,23 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../Backend/assets/img/favicon.png" rel="icon">
+  <link href="../Backend/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/owl.carousel/../Backend/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../Backend/assets/vendor/aos/aos.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="../Backend/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Bethany - v2.2.0
@@ -52,7 +52,7 @@
         <div class="logo mr-auto" style="background-color:#53c68c;">
           <h1 class="text-light"><a href="outside_index.php"><span>Junior Add Logo Here</span></a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
-          <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+          <!-- <a href="index.html"><img src="../Backend/assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>
 
         <nav class="nav-menu" style="background-color:#53c68c;">
