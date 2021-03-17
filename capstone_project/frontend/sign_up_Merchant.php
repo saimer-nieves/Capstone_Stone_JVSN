c05de3d0ef74e757674f7a1f9647dd17ec6323c7
unknown <jjvoltaire@email.neit.edu>
2021-03-17 02:08:44 -0400

    clean page

--- capstone_project/frontend/sign_up_Merchant.php
+++ capstone_project/frontend/sign_up_Merchant.php
@@ -411,30 +411,9 @@ height: 230px;
 
 	   
 	</div>
-    <footer id="footer" style="margin-top:100px; background-color:#53C68C">
-
-  
-
-<div class="container d-md-flex py-4">
-
-  <div class="mr-md-auto text-center text-md-left">
-    <div class="copyright">
-      &copy; Copyright <strong><span>Junior V| Saimer N</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-     
-      Designed by Junior V. & Saimer N NEIT</a>
-    </div>
-  </div>
-  <div class="social-links text-center text-md-right pt-3 pt-md-0">
-    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-  </div>
-</div>
-</footer><!-- End Footer -->
+   <?php
+    include "../includes/footer.php";
+   ?>
 </body>
 	<!--   Core JS Files   -->
 	<script src="assets_login/js/jquery-2.2.4.min.js" type="text/javascript"></script>
